8d9a0695d401bf3aa6e72d03f8c586a2296fb8fe
Kye Browning <kye.browning@gmail.com>
2020-09-29 17:30:03 -0500

    Added take off button functionality

--- scripts.js
+++ scripts.js
@@ -1,2 +1,32 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+let takeoff = null;
+let flightStatus = null;
+let dashboardBackground = null;
+let shuttleHeight = null;
+// let landing = null;
+
+function init(){
+    takeoff = document.getElementById('takeoff');
+    flightStatus = document.getElementById('flightStatus');
+    dashboardBackground = document.getElementById('shuttleBackground');
+    shuttleHeight = document.getElementById('spaceShuttleHeight');
+    landing = document.getElementById('landing');
+
+
+    takeoff.addEventListener('click', function(event){
+        let confirmation = window.confirm('Confirm that the shuttle is ready for takeoff.');
+        if(confirmation){
+            flightStatus.innerHTML = 'Shuttle in flight.';
+            dashboardBackground.style.backgroundColor = 'blue';
+            shuttleHeight.innerHTML = 10000;
+        };
+    });
+
+    // landing.addEventListener('click', function(event){
+        // let alert = window.alert('The shuttle is landing. Landing gear engaged.')
+
+    // });
+}
+
+window.onload = init;
